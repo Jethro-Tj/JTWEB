d8555b598df7804b0f89d313a3733bc812d117ea
Jethro <jethrotedjosoesilo@gmail.com>
2024-01-13 22:43:48 -0500

    Add main template

--- src/App.js
+++ src/App.js
@@ -4,20 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="LeftB">
+        <h1 className="Title">JETHRO TEDJOSOESILO</h1>
+        <h2 className="word">I am a Math Honours student that specializes in Computer Science</h2>
+      </div>
+
+      <div className="RightB">
+        <div className="bottomright">LinkedIn</div>
+      </div>
+      
     </div>
   );
 }
